2362cac2fd2cab4d76d472bcd97e22906f9f3d23
Daniel Correa <danccorrrea@gmail.com>
2022-09-30 21:54:11 -0300

    Desafio C# - Bootcamp Pottencial .NET Developer

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using DesafioPOO.Models;
+
+Console.WriteLine("Nokia");
+Smartphone nokia = new Nokia("9988776655", "N95", "147852369", 64);
+nokia.ReceberLigacao();
+nokia.Ligar();
+nokia.InstalarAplicativo("Snakes");
+
+Console.WriteLine("----------------------------");
+
+Console.WriteLine("iPhone");
+Smartphone iphone = new Iphone("9988776655", "11 S Pro", "9846232679", 128);
+iphone.ReceberLigacao();
+iphone.Ligar();
+iphone.InstalarAplicativo("Face Time");
